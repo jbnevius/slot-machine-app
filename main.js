5c32508cc8ba1106c77c8dafe42b7fa8208dd447
Josh Nevius <joshnevius92@gmail.com>
2024-11-26 07:52:21 -0500

    Completed variables and functions for slot machine spin capability and functioning with randomizer indexes.

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-// Allows the user to deposit money
-
 const prompt = require("prompt-sync")();
+
+// Global variables set for the rows and columns for the slot machine
+const ROWS = 3;
+const COLS = 3;
+
+// Global variable that sets key-value count
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+// Global variable that sets key-value multiplier 
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+// Allows the user to deposit money
 
 const deposit = () => {
     while (true) {
@@ -45,6 +65,29 @@ const getBet = (balance, lines) => {
     }
 }
 
+// This block enables a slot spin with the ability to randomize indexes based on the global variables set above
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [[], [], []];
+    for (let i =0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
+
+const reels = spin();
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
