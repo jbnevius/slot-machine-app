b80b312b3a5e4dc6f1383ee2e18f9ca770fbcc7a
Josh Nevius <joshnevius92@gmail.com>
2024-11-25 22:45:15 -0500

    Added the ability for a user to bet on a certain number of lines

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Working on user ability to deposit money into slot machine
+// Allows the user to deposit money
 
 const prompt = require("prompt-sync")();
 
@@ -15,4 +15,20 @@ const deposit = () => {
     }
 };
 
-const depositAmount = deposit();
+// Lets the user select the number of lines they'd like to bet on between 1 - 3 
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines you'd like to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+let depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
